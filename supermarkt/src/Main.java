76001dfa011170393d5ebb8080d23a5bf57e0c48
sjoerdjansz <sjoerdjansz@gmail.com>
2025-09-24 20:05:08 +0200

    base layout: classes and scanner in main

---
+++ supermarkt/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter username");
+    String username = scanner.nextLine();
+    System.out.println("Your name is: " + username);
+}
